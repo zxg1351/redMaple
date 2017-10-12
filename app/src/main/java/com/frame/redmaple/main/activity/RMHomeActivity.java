9c960d0c18da3e7c17b4da55f136ab66acbc7012
zxg1351 <zxg_546789@163.com>
2017-10-12 16:57:45 +0800

    20171012

--- app/src/main/java/com/frame/redmaple/main/activity/RMHomeActivity.java
+++ app/src/main/java/com/frame/redmaple/main/activity/RMHomeActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.frame.redmaple.R;
+import com.frame.redmaple.base.common.CommonActivity;
 import com.frame.redmaple.main.fragment.Fragment_Case;
 import com.frame.redmaple.main.fragment.Fragment_Home;
 import com.frame.redmaple.main.fragment.Fragment_Me;
@@ -19,7 +26,7 @@ import com.frame.redmaple.main.fragment.Fragment_Me;
  * Created by Administrator on 2017/9/25.
  */
 
-public class RMHomeActivity extends FragmentActivity {
+public class RMHomeActivity extends AppCompatActivity {
     //    private TextView mTextMessage;
     private BottomNavigationView navigation;
     private FragmentManager fragmentManager;
@@ -32,11 +39,9 @@ public class RMHomeActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home_test);
 //        mTextMessage = (TextView) findViewById(R.id.message);
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
-
-
         fragmentManager = getSupportFragmentManager();
         transaction = fragmentManager.beginTransaction();
 
@@ -46,6 +51,26 @@ public class RMHomeActivity extends FragmentActivity {
             transaction.addToBackStack(null);
             transaction.commit();
         }
+        initView();
+//        findViewById(R.id.toolbar);
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+//        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.rootLayout);
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                switch (item.getItemId()) {
+//
+//                    case R.id.item_one:
+//                        CommonActivity.ToastUtil3.showToast(getBaseContext(), "测试退出成功");
+//
+//                        break;
+//                }
+//                item.setChecked(true);//点击了把它设为选中状态
+//                mDrawerLayout.closeDrawers();//关闭抽屉
+//                return true;
+//            }
+//        });
 
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -71,6 +96,43 @@ public class RMHomeActivity extends FragmentActivity {
         });
 
 
+    }
+
+
+    private void initView() {
+        final Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+//        toolbar_title = (TextView) findViewById(R.id.toolbar_title);
+//        toolbar_title.setText("首页");
+
+        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.rootLayout);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.syncState();//初始化状态
+        mToolbar.setNavigationIcon(R.mipmap.icon_skid);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.item_one:
+                        CommonActivity.ToastUtil3.showToast(RMHomeActivity.this, "点击退出成功");
+
+                        break;
+
+                }
+                menuItem.setChecked(true);//点击了把它设为选中状态
+                mDrawerLayout.closeDrawers();//关闭抽屉
+                return true;
+            }
+        });
     }
 
     /**
---
+++ app/src/main/java/com/frame/redmaple/main/selectorPicture/PictureSelectorActivity.java
@@ -0,0 +1,20 @@
+package com.frame.redmaple.main.selectorPicture;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.frame.redmaple.base.common.CommonActivity;
+
+/**
+ * Created by Administrator on 2017/10/12.
+ */
+
+public class PictureSelectorActivity extends CommonActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
